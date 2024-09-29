435d04f6ff946ee79ef322f052690b068cb3ca53
sarahschuller <93050611+sarahschuller@users.noreply.github.com>
2024-09-29 18:22:54 -0400

    update home page content

--- wp-content/themes/gryzzl/index.php
+++ wp-content/themes/gryzzl/index.php
@@ -1,12 +1,12 @@
 <?php
 get_header();
-
-while (have_posts()) {
-    the_post(); ?>
-    <h2><a href="<?php the_permalink(); ?>"><?php the_title() ?></a></h2>
-    <?php the_content(); ?>
-
-<?php }
-
-get_footer();
 ?>
+
+<section class="main__heading">
+    <div class="main__heading-name">
+        <h1>Gryzzl</h1>
+    </div>
+    <div class="main__heading-tagline">
+        <h2>A simple starter theme</h2>
+    </div>
+</section>
